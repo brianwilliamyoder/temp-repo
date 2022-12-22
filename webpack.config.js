6e43af6ffbbb05747ef949fadf8887c69aa147bc
Brian Yoder <brianyoder@gmail.com>
2022-12-21 18:41:15 -0800

    install ESLint, ESLintWebpackPlugin, update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,8 +9,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: './dist'
+  },
   devtool: 'eval-source-map',
   plugins: [
+    new ESLintWebpackPlugin(),
     new CleanWebpackPlugin({
 verbose: true
     }), 
