c205a70c7bb830702986a4af45a1a1ec05aee197
Brian Yoder <brianyoder@gmail.com>
2022-12-21 18:32:35 -0800

    add HTMLWebpackPluin, update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +7,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+    plugins: [
+    new HtmlWebpackPlugin({
+      title: 'age-calculator',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
@@ -18,3 +26,4 @@ module.exports = {
     ]
   }
 };
+
