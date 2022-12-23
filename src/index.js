a65a99b814ce9bd654492b9e04a56153f0e209e6
Brian Yoder <brianyoder@gmail.com>
2022-12-23 14:48:55 -0800

    update index.html, add ui functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { AgeCalculator } from './calculator.js';
+
+
+function handleAgeForm(event) {
+  event.preventDefault();
+  const age = document.querySelector("#your-age").value;
+  const person = new AgeCalculator(age);
+  const futureAge = document.querySelector("#future-age").value;
+  const pastAge = document.querySelector("#past-age").value;
+  person.convertEarthAge(futureAge, pastAge);
+  person.convertMercuryAge(futureAge, pastAge);
+  person.convertVenusAge(futureAge, pastAge);
+  person.convertMarsAge(futureAge, pastAge);
+  person.convertJupiterAge(futureAge, pastAge);
+  displayResults();
+}
+
+function displayResults() {
+  
+
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("#form").addEventListener("submit", handleAgeForm);
+});
