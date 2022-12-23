7e4a7c5285d4fed5c6af6b920e54a07be72aa94f
Brian Yoder <brianyoder@gmail.com>
2022-12-23 10:50:45 -0800

    PASS 7th test, refactor all planet conversion methods to include yearsuntil and yearssince conversions

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -26,24 +26,25 @@ describe('AgeCalculator', () => {
 
   test('should calculate a persons venus age', () => {
     person.convertVenusAge(person.age);
-    expect(person.convertVenusAge(person.age)).toEqual(59.67741935483871);
+    expect(person.planetAge.venus).toEqual("59.68");
   
   })
 
   test('should calculate a persons mars age', () => {
     person.convertMarsAge(person.age);
-    expect(person.convertMarsAge(person.age)).toEqual(19.680851063829788);
+    expect(person.planetAge.mars).toEqual("19.68");
   
   })
 
   test('should calculate a persons jupiter age', () => {
     person.convertJupiterAge(person.age);
-    expect(person.convertJupiterAge(person.age)).toEqual(3.119730185497471);
+    expect(person.planetAge.jupiter).toEqual("3.12");
   })
 
   test('should calculate years until a birthday in earth years', () => {
-    person.yearsUntilEarth(39);
-    expect(person.yearsUntilEarth(39)).toEqual(2);
+    person.convertEarthAge(39, 36);
+    expect(person.yearsUntil.earth).toEqual(2);
+    expect(person.yearsSince.earth).toEqual(1);
   })
 
   test('should calculate a persons age on all planets, including years since, and years until a given age', () => {
@@ -52,16 +53,16 @@ describe('AgeCalculator', () => {
   person.convertVenusAge(39, 36);
   person.convertMarsAge(39, 36);
   person.convertJupiterAge(39, 36)
-  expect(person.yearsUntil.earth).toEqual("2");
-  expect(person.yearsSince.earth).toEqual("1");
-  expect(person.yearsUntil.mercury).toEqual();
-  expect(person.yearsSince.mercury).toEqual();
-  expect(person.yearsUntil.venus).toEqual();
-  expect(person.yearsSince.venus).toEqual();
-  expect(person.yearsUntil.mars).toEqual();
-  expect(person.yearsSince.mars).toEqual();
-  expect(person.yearsUntil.jupiter).toEqual();
-  expect(person.yearsSince.jupiter).toEqual();
+  expect(person.yearsUntil.earth).toEqual(2);
+  expect(person.yearsSince.earth).toEqual(1);
+  expect(person.yearsUntil.mercury).toEqual("8.33");
+  expect(person.yearsSince.mercury).toEqual("4.17");
+  expect(person.yearsUntil.venus).toEqual("3.23");
+  expect(person.yearsSince.venus).toEqual("1.61");
+  expect(person.yearsUntil.mars).toEqual("1.06");
+  expect(person.yearsSince.mars).toEqual("0.53");
+  expect(person.yearsUntil.jupiter).toEqual("0.17");
+  expect(person.yearsSince.jupiter).toEqual("0.08");
   
 
   })
