61bd2a37d478b117be63a179353121adf33e9d2b
Brian Yoder <brianyoder@gmail.com>
2022-12-22 18:35:38 -0800

    write 2nd test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,10 +2,21 @@ import { AgeCalculator } from "../src/calculator.js";
 const { TestScheduler } = require("jest")
 
 describe('AgeCalculator', () => {
+  let person;
+
+  beforeEach(() => {
+    person = new AgeCalculator("Brian", 37);
+  });
+
   test('should create a new calculator object with properties', () => {
-    const person = new AgeCalculator("Brian", 37);
     expect(person.name).toEqual("Brian");
     expect(person.age).toEqual(37);
     expect(person.planetAge).toEqual({});
   })
+
+  test('should calculate a persons mercury age', () => {
+    expect(person.planetAge.mercury).toEqual(154.16);
+
+  })
 })
+
