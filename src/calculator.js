e6917af31ffffe42bee9ad3ae67c338444761e5f
Brian Yoder <brianyoder@gmail.com>
2022-12-22 19:52:49 -0800

    add remaining planets to converPlanetAge() method

--- src/calculator.js
+++ src/calculator.js
@@ -7,8 +7,13 @@ export class AgeCalculator {
 
   convertPlanetAge() {
     let mercAge =  this.age / .24;
+    let venusAge = this.age / .62;
+    let marsAge = this.age / 1.88;
+    let jupiterAge = this.age / 11.86;
     this.planetAge.mercury = mercAge.toFixed(2);
-    
+    this.planetAge.venus = venusAge.toFixed(2);
+    this.planetAge.mars = marsAge.toFixed(2);
+    this.planetAge.jupiter = jupiterAge.toFixed(2);
   }
 
 }
