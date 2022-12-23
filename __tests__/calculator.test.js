da4221dcbd7cb58c60d70f47706075fe75cc733f
Brian Yoder <brianyoder@gmail.com>
2022-12-23 09:17:22 -0800

    delete 7th test, refactor test 2

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -16,27 +16,29 @@ describe('AgeCalculator', () => {
   })
 
 
-  test('should calculate a persons mercury age', () => {
-    person.convertMercuryAge();
-    expect(person.convertMercuryAge()).toEqual(154.16666666666669);
+  test('should calculate a persons mercury age, years until age, and years since age', () => {
+    person.convertMercuryAge(person.age, untilAge, sinceAge);
+    expect(person.age).toEqual(154.17);
+    expect(person.yearsUntil.mercury).toEqual();
+    expect(person.yearsSince.mercury).toEqual();
 
   })
 
   test('should calculate a persons venus age', () => {
-    person.convertVenusAge();
-    expect(person.convertVenusAge()).toEqual(59.67741935483871);
+    person.convertVenusAge(person.age);
+    expect(person.convertVenusAge(person.age)).toEqual(59.67741935483871);
   
   })
 
   test('should calculate a persons mars age', () => {
-    person.convertMarsAge();
-    expect(person.convertMarsAge()).toEqual(19.680851063829788);
+    person.convertMarsAge(person.age);
+    expect(person.convertMarsAge(person.age)).toEqual(19.680851063829788);
   
   })
 
   test('should calculate a persons jupiter age', () => {
-    person.convertJupiterAge();
-    expect(person.convertJupiterAge()).toEqual(3.119730185497471);
+    person.convertJupiterAge(person.age);
+    expect(person.convertJupiterAge(person.age)).toEqual(3.119730185497471);
   })
 
   test('should calculate years until a birthday in earth years', () => {
@@ -44,14 +46,5 @@ describe('AgeCalculator', () => {
     expect(person.yearsUntilEarth(39)).toEqual(2);
   })
 
-  test('should calculate years until a birthday in mercury, venus, mars, and juipter years', () => {
-    person.yearsUntiLMercury(39);
-    person.yearsUntilVenus(39);
-    person.yearsUntilMars(39);
-    person.yearsUntilJupiter(39);
-    expect(person.yearsUntilMercury(39)).toEqual();
-    expect(person.yearsUntilVenus(39)).toEqual();
-    expect(person.yearsUntilMars(39)).toEqual();
-    expect(person.yearsUntilJupiter(39)).toEqual();
-  })
+
 })
